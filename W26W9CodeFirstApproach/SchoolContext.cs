cc97e3739e3f2e74a9d5d8c088be1a716f166cf3
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-03-13 09:53:00 -0400

    context class

---
+++ W26W9CodeFirstApproach/SchoolContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace W26W9CodeFirstApproach
+{
+    // context class
+    public class SchoolContext : DbContext
+    {
+        // connection string - REQUIRED
+        // read the conn str from the App.config file
+        // override the OnConfiguring method
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["School"].ConnectionString;
+            optionsBuilder.UseSqlServer(connStr);
+        }
+
+
+        // define entity sets - REQUIRED
+        // use DbSet properties
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Standard> Standards { get; set; }
+
+
+        // data seeding - OPTIONAL
+        // override the OnModelCreating method
+
+    }
+}
